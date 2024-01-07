9607c23266f43d4eb20269954f257783677a4acb
Adam Daniels <adam@mediadrive.ca>
2024-01-06 21:45:17 -0500

    Extract methods

--- lib/asset_digest/digester.rb
+++ lib/asset_digest/digester.rb
@@ -14,16 +14,25 @@ module AssetDigest
 
     def digest_all(source)
       Pathname.new(source).glob("**/*").each do |source_path|
-        sha = Digest::SHA256.hexdigest(source_path.read)
-
-        ext = source_path.extname
-        filename = source_path.basename.to_s.chomp(ext)
-
-        output = "#{filename}-#{sha}#{ext}"
-        destination_path = Pathname.new(destination).join(output)
+        destination_path = generate_destination_path(source_path)
 
         FileUtils.cp(source_path, destination_path)
       end
+    end
+
+    private
+
+    def generate_digest(source)
+      Digest::SHA256.hexdigest(source.read)
+    end
+
+    def generate_destination_path(source_path)
+      sha = generate_digest(source_path)
+      ext = source_path.extname
+      filename = source_path.basename.to_s.chomp(ext)
+
+      output = "#{filename}-#{sha}#{ext}"
+      Pathname.new(destination).join(output)
     end
   end
 end

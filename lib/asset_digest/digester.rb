95e6c0687817e9f2d29d23d01ad83e3ce6602950
Adam Daniels <adam@mediadrive.ca>
2024-01-06 22:02:23 -0500

    Generate digest paths correctly

--- lib/asset_digest/digester.rb
+++ lib/asset_digest/digester.rb
@@ -18,7 +18,7 @@ module AssetDigest
       Pathname.new(source).glob("**/*").each do |source_path|
         next if source_path.directory?
 
-        destination_path = generate_destination_path(source_path)
+        destination_path = generate_destination_path(source, source_path)
         ensure_folder_exists(destination_path)
         update_manifest(source, source_path, destination_path)
         FileUtils.cp(source_path, destination_path)
@@ -31,10 +31,10 @@ module AssetDigest
       Digest::SHA256.hexdigest(source.read)
     end
 
-    def generate_destination_path(source_path)
+    def generate_destination_path(source, source_path)
       sha = generate_digest(source_path)
       ext = source_path.extname
-      filename = source_path.basename(ext)
+      filename = source_path.relative_path_from(source).to_s.chomp(ext)
 
       output = "#{filename}-#{sha}#{ext}"
       Pathname.new(destination).join(output)
@@ -45,7 +45,8 @@ module AssetDigest
     end
 
     def update_manifest(source, source_path, destination_path)
-      @manifest[source_path.relative_path_from(source).to_s] = destination_path.relative_path_from(destination).to_s
+      @manifest[source_path.relative_path_from(source).to_s] =
+        destination_path.relative_path_from(destination).to_s
     end
   end
 end

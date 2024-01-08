1d389b13e0089a586df9e5d6c95b4f1f43caa8fa
Adam Daniels <adam@mediadrive.ca>
2024-01-08 09:17:11 -0500

    Block form of mktmpdir to ensure cleanup

--- test/test_asset_digest.rb
+++ test/test_asset_digest.rb
@@ -8,25 +8,26 @@ class TestAssetDigest < Minitest::Test
   end
 
   def test_performing_digest
-    destination = Dir.mktmpdir
-    source = File.join(__dir__, "support/assets")
-    digester = AssetDigest::Digester.new(
-      source: source,
-      destination: destination,
-      manifest_path: File.join(destination, "manifest.json")
-    )
+    Dir.mktmpdir do |destination|
+      source = File.join(__dir__, "support/assets")
+      digester = AssetDigest::Digester.new(
+        source: source,
+        destination: destination,
+        manifest_path: File.join(destination, "manifest.json")
+      )
 
-    assert digester.digest_all
-    assert File.exist?(destination + "/app-b7ca7577aa.css")
-    expected_manifest = {
-      "app.css" => "app-b7ca7577aa.css",
-      "css/styles.css" => "css/styles-477e369aa7.css"
-    }
-    assert_equal expected_manifest, digester.manifest.to_h
+      assert digester.digest_all
+      assert File.exist?(destination + "/app-b7ca7577aa.css")
+      expected_manifest = {
+        "app.css" => "app-b7ca7577aa.css",
+        "css/styles.css" => "css/styles-477e369aa7.css"
+      }
+      assert_equal expected_manifest, digester.manifest.to_h
 
-    manifest_io = StringIO.new
-    digester.manifest.write(manifest_io)
+      manifest_io = StringIO.new
+      digester.manifest.write(manifest_io)
 
-    refute_nil manifest_io.string
+      refute_nil manifest_io.string
+    end
   end
 end

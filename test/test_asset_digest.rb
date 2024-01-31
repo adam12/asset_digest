3c86dab199ae64524d911cb00bda152c69e0e5ce
Adam Daniels <adam@mediadrive.ca>
2024-01-31 13:10:37 -0500

    Test with second asset source

--- test/test_asset_digest.rb
+++ test/test_asset_digest.rb
@@ -10,18 +10,22 @@ class TestAssetDigest < Minitest::Test
   def test_performing_digest
     Dir.mktmpdir do |destination|
       source = File.join(__dir__, "support/assets")
+      other_source = File.join(__dir__, "support/assets-other")
       digester = AssetDigest::Digester.new(
         destination: destination,
         manifest_path: File.join(destination, "manifest.json")
       )
       digester.add_source(source)
+      digester.add_source(other_source)
 
       assert digester.digest_all
       assert File.exist?(destination + "/app-b7ca7577aa.css")
       expected_manifest = {
         "app.css" => "app-b7ca7577aa.css",
         "css/styles.css" => "css/styles-477e369aa7.css",
-        "js/app.js" => "js/app-da39a3ee5e.js"
+        "js/app.js" => "js/app-da39a3ee5e.js",
+        "css/other-2.css" => "css/other-2-d98a366516.css",
+        "other.css" => "other-ec5d074194.css"
       }
       assert_equal expected_manifest, digester.manifest.to_h
 
